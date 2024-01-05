e6821aa7e49bff988f43ee81c4da3439f41bc66f
Audun Bjørkøy <audun@tind.io>
2024-01-05 19:50:44 +0100

    Create entrypoint.sh script

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run the command to generate the unicast_hosts.txt file
+dig SRV elasticsearch.elasticsearch.internal | awk '/IN SRV/ {print $8 ":" $7}' | sed 's/\.:/:/g' > /usr/share/elasticsearch/config/unicast_hosts.txt
+
+# Check if the file was created successfully
+if [ ! -s /usr/share/elasticsearch/config/unicast_hosts.txt ]; then
+    echo "Failed to create unicast_hosts.txt, exiting."
+    exit 1
+fi
+
+# Execute the original Elasticsearch entrypoint script with any arguments passed to this script
+exec /usr/local/bin/docker-entrypoint.sh "$@"
